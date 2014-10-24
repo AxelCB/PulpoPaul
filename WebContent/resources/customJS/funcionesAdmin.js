e5dcaaf6ddc95d448c9946985dd90cf4a3505b83
Axel Collard Bovy <axcbovy@gmail.com>
2014-10-23 21:55:14 -0300

    ultimo commit antes de la entrega. detalles de la vista de gestion de eventos

--- WebContent/resources/customJS/funcionesAdmin.js
+++ WebContent/resources/customJS/funcionesAdmin.js
@@ -1,3 +1,11 @@
 var mostrarModalNuevoUsuario = function(){
 	$('#nuevo-usuario').modal('show');
 }
+
+var mostrarModalNuevoEvento = function(){
+	$('#nuevo-evento').modal('show');
+}
+
+var mostrarModalEliminar= function(){
+	$('#eliminar-elemento').modal('show');
+}
